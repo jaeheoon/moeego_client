420f27e044312cc57e9b9d93189b250d40d80077
gustlr887 <gustlr887@naver.com>
2024-11-19 12:37:27 +0900

    [FE] Feat: 메인페이지 CSS 작성

--- src/components/mainpage/CleaningSection.jsx
+++ src/components/mainpage/CleaningSection.jsx
@@ -5,10 +5,22 @@ function CleaningSection() {
     <div className="keywordWrap">
         <h2>쓱싹쓱싹 청소하는 날</h2>
         <div className="keyword-images">
-            <div>정리정돈 서비스</div>
-            <div>청소/세탁소 청소</div>
-            <div>아파트 청소</div>
-            <div>폐기물 처리</div>
+          <figure>
+            <img src="./src/image/cleaning.png" alt="청소하는날"/>
+          <figcaption>정리수납 컨설팅</figcaption>
+          </figure>
+          <figure>
+            <img src="./src/image/cleaning.png" alt="청소하는날"/>
+          <figcaption>정리수납 컨설팅</figcaption>
+          </figure>
+          <figure>
+            <img src="./src/image/cleaning.png" alt="청소하는날"/>
+          <figcaption>정리수납 컨설팅</figcaption>
+          </figure>
+          <figure>
+            <img src="./src/image/cleaning.png" alt="청소하는날"/>
+          <figcaption>정리수납 컨설팅</figcaption>
+          </figure>
         </div>
     </div>
   );
--- src/components/mainpage/MainBanner.jsx
+++ src/components/mainpage/MainBanner.jsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 function MainBanner() {
   return (
     <div className="mainBannerWrap">
-      <h1>내가 찾던 모든 전문가, 모이고에서 한눈에!</h1>
-      <div className="searchCategoryWrap">
-            <img src=""/>
-            <input type="text" placeholder="검색어 입력" />
+        <h1>내가 찾던 모든 전문가, 모이고에서 한눈에!</h1>
+        <div className="searchCategoryWrap">
+            <div className="searchBarWrap">
+                <img src="./src/image/search.png" alt="검색버튼"/>
+                <input type="text" placeholder="검색어 입력" />
+            </div>
             <div className="categories">
                 <Link to='/category/home'>
                     <div className="category-item">
