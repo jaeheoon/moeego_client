9ad065bc4ea2048c87a5779fd6f33a182f45c344
codelily <coldelily98@naver.com>
2024-11-19 12:28:07 +0900

    [FE] Feat: 마이페이지 CSS 작성

--- src/components/mypage/MyPage.jsx
+++ src/components/mypage/MyPage.jsx
@@ -1,64 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import '../../css/mypage/MyPage.css';
+import '../../css/mypage/MyPage.css';
 
 const MyPage = () => {
     return (
         <div className='MyPage'>
-            <h1>마이페이지</h1>
-            <div className='ProfileContainer'>
-                <Link to='/mypage/account' className='ProfileSettingPage'>
-                    <div className='ProfileImage'>
-                        <img src="" alt="" />
-                    </div>
-                    <div className='ProfileInfo'>
-                        <div>ooo 고객님</div>
-                        <div><img src="" alt="" /></div>
-                        <div>moeego@moeego.com</div>
-                    </div>
-                    <div className='ProfileButton'>
-                        <input type="button" value="계정설정" />
-                    </div>
-                </Link>
-            </div>
-            <hr />
-
-            <div className='SearchProContainer'>
-                <h3>달인찾기</h3>
-                <div>
-                    <Link to='/mypage/likepro' className='SearchProLink'>
-                        <div>찜한 달인</div>
-                        <div><img src="./src/image/next_icon.png" alt="nextIcon" /></div>
+            <div className='MyPageWrap'>
+                <h1>마이페이지</h1>
+                <div className='ProfileContainer'>
+                    <Link to='/mypage/account' className='ProfileSettingPage'>
+                        <div className='ProfileImage'>
+                            <img src="../src/image/profile_default.png" alt="profile" />
+                        </div>
+                        <div className='ProfileInfo'>
+                            <div className='NickName'>ooo 고객님</div>
+                            <div className='MailWrap'>
+                                <div className="SNSWrap"><img src="../src/image/naver_sns.png" alt="naver" /></div>
+                                <div className='Email'>moeego@moeego.com</div>
+                            </div>
+                        </div>
+                        <div className='ProfileButton'>
+                            <input type="button" value="계정설정" />
+                        </div>
                     </Link>
                 </div>
-            </div>
-            <hr />
+                <hr />
 
-            <div className='ActivityDetailsContainer'>
-                <h3>모이고 활동내역</h3>
-                <div>
-                    <Link to='/mypage/reservation' className='ActivityDetailsLink'>
-                        <div>모이고 예약 내역</div>
-                        <div><img src="./src/image/next_icon.png" alt="nextIcon" /></div>
-                    </Link>
-                    <Link to='' className='MyReviewsLink'>
-                        <div>모이고 리뷰 내역</div>
-                        <div><img src="./src/image/next_icon.png" alt="nextIcon" /></div>
-                    </Link>
+                <div className='Container'>
+                    <h3>달인찾기</h3>
+                    <div>
+                        <Link to='/mypage/likepro' className='Link'>
+                            <div className="Title">찜한 달인</div>
+                            <div className='next'><img src="./src/image/next_icon.png" alt="nextIcon" /></div>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-            <hr />
+                <hr />
 
-            <div className='ArticleHistoryContainer'>
-                <h3>커뮤니티</h3>
-                <div>
-                    <Link to='/mypage/myhistory' className='ArticleHistoryLink'>
-                        <div>커뮤니티 작성 글/댓글</div>
-                        <div><img src="./src/image/next_icon.png" alt="nextIcon" /></div>
-                    </Link>
+                <div className='Container'>
+                    <h3>모이고 활동내역</h3>
+                    <div>
+                        <Link to='/mypage/reservation' className='Link'>
+                            <div className="Title">모이고 예약 내역</div>
+                            <div className='next'><img src="./src/image/next_icon.png" alt="nextIcon" /></div>
+                        </Link>
+                        <br />
+                        <Link to='' className='Link'>
+                            <div className="Title">모이고 리뷰 내역</div>
+                            <div className='next'><img src="./src/image/next_icon.png" alt="nextIcon" /></div>
+                        </Link>
+                    </div>
                 </div>
+                <hr />
+
+                <div className='Container'>
+                    <h3>커뮤니티</h3>
+                    <div>
+                        <Link to='/mypage/myhistory' className='Link'>
+                            <div className="Title">커뮤니티 작성 글/댓글</div>
+                            <div className='next'><img src="./src/image/next_icon.png" alt="nextIcon" /></div>
+                        </Link>
+                    </div>
+                </div>
+                <hr />
             </div>
-            <hr />
         </div>
     );
 };
