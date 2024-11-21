839d045b282c06cfac3e2444f36415ae32653a28
xoxosmj <81194812+xoxosmj@users.noreply.github.com>
2024-11-21 10:26:46 +0900

    CommunityViewPage CSS 구현(임시) (#54)

--- src/components/articles/viewpage/PopularPostList.jsx
+++ src/components/articles/viewpage/PopularPostList.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import "/src/css/articles/PopularPostList.css";
 
 const PopularPostList = () => {
     return (
-        <div>
-            <ul>
-                <li>
-                    <a></a>
-                    <a></a>
-                    <a></a>
-                </li>
-            </ul>
-        </div>
+        <section>
+            <div>
+                <h3>지금 가장 뜨거운🔥커뮤니티 게시글</h3>
+
+                <><Link to="">
+                    <div className='PopularPostListContainer'>
+                        <div className='AllList'>
+                            <div className='AllWrap'>
+                                <div className='viewWrap'>
+                                    <div className='titleWrap'>
+                                        <div>제목</div>
+                                    </div>
+                                    <div className="contentWrap">
+                                        <div>글내용1, 글내용2, 글내용3, 글내용4, 글내용5</div>
+                                    </div>
+                                    <div className='imageWrap'>
+                                        <div><img src='../src/image/view_icon.svg' alt='view'/><span>100</span></div>
+                                        <div><img src='../src/image/chat_icon.svg' alt='chat'/><span>100</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+                </>
+            </div>
+        </section>
     );
 };
 
--- src/components/articles/viewpage/PostDetail.jsx
+++ src/components/articles/viewpage/PostDetail.jsx
@@ -3,21 +3,24 @@ import PostHeader from './PostHeader';
 import PostContent from './PostContent';
 import PostReactState from './PostReactState';
 import PostComment from './PostComment';
-import PopularPost from './PopularPost';
 import '/src/css/articles/PostDetail.css'
+import PopularPostList from "./PopularPostList.jsx";
 
 const PostDetail = () => {
     return (
         <div className="post-detail">
             <div className="post-detail-wrap">
                 <section className="post-detail-container">
-                    <PostHeader />
+                    <PostHeader/>
                     <br/>
-                    <PostContent />
-                    <PostReactState />
-                    <hr />
-                    <PostComment />
-                    <PopularPost />
+                    <PostContent/>
+                    <PostReactState/>
+                    <br/>
+                    <PostComment/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <PopularPostList/>
                 </section>
             </div>
         </div>
