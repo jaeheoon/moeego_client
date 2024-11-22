ce0330b7f1de0e1d256230aef4dc18ddccddfeb5
xoxosmj <81194812+xoxosmj@users.noreply.github.com>
2024-11-22 10:04:37 +0900

    CommunityViewPage CSS 구현 (#87)

--- src/components/articles/Write.jsx
+++ src/components/articles/Write.jsx
@@ -1,67 +1,53 @@
 import React from 'react';
+import '/src/css/articles/Write.css';
 
 const Write = () => {
     return (
-        <div> 
-            <div> 
-                <div> 
-                    <selection> 
-                        <div> 
-                            <div> 
-                                <select> 
-                                    <option disabled="disabled">주제 선택</option>
-                                    <option>고수 게시판</option>
-                                    <option>자유 게시판</option>
-                                    <option>QnA</option>
-                                </select>
-                                <button disabled="disabled">등록</button>
-                            </div>
-                        </div>
+        <div className="write-container">
+            {/* 선택 및 등록 버튼 */}
+            <div className="select-container">
+                <select>
+                    <option selected disabled>주제 선택</option>
+                    <option>고수 게시판</option>
+                    <option>자유 게시판</option>
+                    <option>QnA</option>
+                </select>
+                <button className="submit-button">
+                    등록
+                </button>
+            </div>
 
-                        <div> 
-                            <div>
-                                <div></div>
-                                <div>
-                                    <input type='file'></input>
-                                    <label>
-                                        <span>No file chosen</span>
-                                    </label>
-                                </div>
-                                <span>0/15</span>
-                            </div>
-                        </div>
-                        
-                        
-                        <div>
-                            <div>
-                                <div>
-                                    <div>
-                                        <label>
-                                            <input type='text' placeholder='제목을 입력해주세요'></input>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div><hr/></div>    
-                                <div>
-                                    <button>
-                                        <span>(선택) 서비스</span>
-                                    </button>
-                                    <button>
-                                        <span>(선택) 지역</span>
-                                    </button>
-                                </div>
-                                <div><hr/></div>
+            {/* 파일 업로드 */}
+            <div className="file-container">
+                <input type="file" id="file-upload" />
+                <label htmlFor="file-upload"><img className={'camera-img'} src='/src/image/camera.png'/></label>
+                <span>No file chosen</span>
+            </div>
 
-                                <div>
-                                    <span>
-                                        <textarea></textarea>
-                                    </span>
-                                    <span>과외 친구, 공동 구매 그룹 , 취미활동까지 함께 할 사람을 찾아보세요. 예) 게임을 좋아하는 분 누구나</span>
-                                </div>
-                            </div>
-                        </div>
-                    </selection>
-                </div>
+            {/* 제목 입력 */}
+            <div className="subject-container">
+                <input type="text" placeholder="제목을 입력해주세요." />
+            </div>
+            <hr />
+
+            {/* 서비스와 지역 선택 */}
+            <div className="service-area-wrap">
+                <button>
+                    <span>(선택) 서비스</span>
+                </button>
+                <button>
+                    <span>(선택) 지역</span>
+                </button>
+
+            </div>
+            <hr />
+
+            {/* 본문 입력 */}
+            <div className="content-container">
+                <textarea placeholder="내용을 입력하세요"></textarea>
+{/*
+                <span>과외 친구, 공동 구매 그룹 , 취미활동까지 함께 할 사람을 찾아보세요. 예) 게임을 좋아하는 분 누구나</span>
+*/}
             </div>
         </div>
     );
