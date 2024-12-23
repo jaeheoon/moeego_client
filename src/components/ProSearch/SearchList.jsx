9262b6677d468d1fd21382c7acba24ecd10fed4d
codelily98 <codelily98@naver.com>
2024-12-23 11:14:47 +0900

    [FE] Fix: Merge Rollback 해결 (#438)
    Co-authored-by: codelily <coldelily98@naver.com>

--- src/components/ProSearch/SearchList.jsx
+++ src/components/ProSearch/SearchList.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const SearchList = ({ item }) => {
+const SearchList = ({ item, proNo }) => {
     const [isToggled, setIsToggled] = useState(false); // 토글 상태 관리
     const navigate = useNavigate();
 
     const handleProViewNavigation = (serviceItem) => {
-        navigate('/pro/proview', {
-            state: { item: item, serviceItem },
-            // search: `?proNo=${item.proNo}`
+        navigate("/pro/proview", {
+            state: { item: item, serviceItem, proNo },
         });
     };
 
@@ -17,23 +16,45 @@ const SearchList = ({ item }) => {
     };
 
     return (
-        <article className='proSearchListWrap'>
-            <div className='proSearchListAWrap' onClick={toggleDetails}>
-                <div className='proSearchListLink'>
-                    <div className='proSearchListContentWrap'>
-                        <div className='proSearchListTitleWrap'>
+        <article className="proSearchListWrap">
+            <div className="proSearchListAWrap" onClick={toggleDetails}>
+                <div className="proSearchListLink">
+                    <div className="proSearchListContentWrap">
+                        <div className="proSearchListTitleWrap">
                             <h3>{item.name}</h3>
                         </div>
-                        <div className='proSearchListProInfoWrap'>
-                            <span>⭐️ {item.star} ({item.reviewCount})</span>
+                        <div className="proSearchListProInfoWrap">
+                            <span>
+                                <span
+                                    style={{
+                                        color: "#f39c12",
+                                        marginRight: "0.25rem",
+                                    }}
+                                >
+                                    <span
+                                        style={{
+                                            color: "#f39c12",
+                                            marginRight: "0.15rem",
+                                        }}
+                                    >
+                                        ★
+                                    </span>
+                                    {Math.floor(item.star * 10) / 10}
+                                </span>
+                                ({item.reviewCount})
+                            </span>
                         </div>
-                        <p className='proSearchListIntro'>{item.oneIntro}</p>
+                        <p className="proSearchListIntro">{item.oneIntro}</p>
                     </div>
                 </div>
-                <div className='proSearchListProfileWrap'>
+                <div className="proSearchListProfileWrap">
                     <div className="user-profile-picture">
                         <img
-                            src={item.profileImage ? `https://kr.object.ncloudstorage.com/moeego/profile/${item.profileImage}` : '/image/default.svg'}
+                            src={
+                                item.profileImage
+                                    ? `https://kr.object.ncloudstorage.com/moeego/profile/${item.profileImage}`
+                                    : "/image/default.svg"
+                            }
                             width={150}
                             height={150}
                             alt={item.name}
@@ -42,19 +63,37 @@ const SearchList = ({ item }) => {
                 </div>
             </div>
             {/* 토글된 상세 정보 부분 */}
-            <div className={`proSearchListDetailWrap ${isToggled ? 'active' : ''}`}>
-                {isToggled && item.proItems.map((serviceItem) => (
-                    <div key={serviceItem.proItemNo} className="servicePage" onClick={() => handleProViewNavigation(serviceItem)}>
-                        <div className="serviceWrap">
-                            <div className='serviceSubject'>
-                                {serviceItem.subject} ({serviceItem.subCategory.subCateName})
-                            </div>
-                            <div className="serviceStar">
-                                ⭐️ {serviceItem.star} ({serviceItem.reviewCount})
+            <div
+                className={`proSearchListDetailWrap ${isToggled ? "active" : ""
+                    }`}
+            >
+                {isToggled &&
+                    item.proItems.map((serviceItem) => (
+                        <div
+                            key={serviceItem.proItemNo}
+                            className="servicePage"
+                            onClick={() => handleProViewNavigation(serviceItem)}
+                        >
+                            <div className="serviceWrap">
+                                <div className="serviceSubject">
+                                    {serviceItem.subject} (
+                                    {serviceItem.subCategory.subCateName})
+                                </div>
+                                <div className="serviceStar">
+                                    <span
+                                        style={{
+                                            color: "#f39c12",
+                                            marginRight: "0.25rem",
+                                        }}
+                                    >
+                                        ★{" "}
+                                        {Math.floor(serviceItem.star * 10) / 10}
+                                    </span>{" "}
+                                    ({serviceItem.reviewCount})
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
             </div>
         </article>
     );
