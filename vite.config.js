d45630e5433ea4b05d98c2d2ce43ef23849a9d99
Jinbeom <91923857+Jeongjinbeom@users.noreply.github.com>
2024-12-27 19:49:04 +0900

    [FE] Ref: vite.config HTTP 추가

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "https://server.moeego.site", // Spring Boot 서버 주소
-        target: "211.188.48.106:8080",
+        target: "http://211.188.48.106:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""), // "/api" 제거
       },
