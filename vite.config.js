d37880afe9af77823453941fe3212d0870c06a8e
codelily98 <codelily98@naver.com>
2024-12-28 18:37:02 +0900

    [FE] Ref: Jenkins 배포 IP 변경
    # 변경한 내용
    - Jenkins 배포 IP 변경

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "http://211.188.56.236", // 호스트를 localhost로 설정
+    host: "http://110.165.18.247", // 호스트를 localhost로 설정
     port: 80, // 포트 설정
 
     proxy: {
       "/api": {
         // target: "https://server.moeego.site", // Spring Boot 서버 주소
-        target: "http://211.188.48.106:8080",
+        target: "http://211.188.56.143:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""), // "/api" 제거
       },
