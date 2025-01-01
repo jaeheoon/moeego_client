060dae1fba26e04dcf92731f1e9ac832829ef966
KangJun <76244088+rkdwns6335@users.noreply.github.com>
2025-01-01 15:41:40 +0900

    [FE] Ref: 관리자 라인차트 date 정렬 수정 (#562)
    * [FE] Ref: 관리자 라인차트 date 정렬 1차 수정
    
    * [FE] Ref: 관리자 라인차트 date 정렬 2차 수정
    
    * [FE] Ref: 관리자 라인차트 date 정렬 3차 수정

--- src/components/admin/LineChart.jsx
+++ src/components/admin/LineChart.jsx
@@ -6,23 +6,33 @@ import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler);
 
 const LineChart = ({ weekMemberData, weekProData, weekLeaveMemberData }) => {
-    const sortAndFilterData = (data) => {
-        return data
-            .map(item => ({ ...item, date: new Date(item.date) })) // 날짜를 Date 객체로 변환
-            .sort((a, b) => a.date - b.date) // 날짜 기준 오름차순 정렬
-            .filter(item => item.date >= new Date(new Date().setDate(new Date().getDate() - 6))); // 최근 7일 데이터 필터링
+    const getPastWeekDates = () => {
+        const dates = [];
+        const today = new Date();
+        for (let i = 7; i >= 0; i--) {
+            const date = new Date(today);
+            date.setDate(today.getDate() - i);
+            dates.push(date.toISOString().split('T')[0]);
+        }
+        return dates;
     };
 
-    const sortedWeekMemberData = [...weekMemberData].sort((a, b) => new Date(a.date) - new Date(b.date)).reverse();
-    const sortedWeekProData = [...weekProData].sort((a, b) => new Date(a.date) - new Date(b.date)).reverse();
-    const sortedWeekLeaveMemberData = [...weekLeaveMemberData].sort((a, b) => new Date(a.date) - new Date(b.date)).reverse();
+    const fillData = (dates, data) => {
+        const dataMap = data.reduce((map, item) => {
+            map[item.date] = item.count;
+            return map;
+        }, {});
+        return dates.map(date => dataMap[date] || 0);
+    };
+
+    const pastWeekDates = getPastWeekDates();
 
     const chartData = {
-        labels: sortedWeekMemberData.map(item => item.date.toISOString().split('T')[0]), // 날짜 (X축)
+        labels: pastWeekDates, // 날짜 (X축)
         datasets: [
             {
                 label: '회원 가입 수',
-                data: sortedWeekMemberData.map(item => item.count), // 가입된 사용자 수 (Y축)
+                data: fillData(pastWeekDates, weekMemberData), // 가입된 사용자 수 (Y축)
                 borderColor: 'rgba(75, 192, 192, 1)', // 선 색상
                 backgroundColor: 'rgba(75, 192, 192, 0.2)', // 선 내부 색상
                 fill: true, // 선 내부 채우기
@@ -30,7 +40,8 @@ const LineChart = ({ weekMemberData, weekProData, weekLeaveMemberData }) => {
             },
             {
                 label: '고수 등록 수',
-                data: sortedWeekProData.map(item => item.count), // 고수 신청 수 (Y축)
+                data: fillData(pastWeekDates, weekProData), // 고수 신청 수 (Y축)
+
                 borderColor: 'rgba(255, 99, 132, 1)', // 고수 신청 선 색상
                 backgroundColor: 'rgba(255, 99, 132, 0.2)', // 선 내부 색상
                 fill: true, // 선 내부 채우기
@@ -38,7 +49,7 @@ const LineChart = ({ weekMemberData, weekProData, weekLeaveMemberData }) => {
             },
             {
                 label: '탈퇴 회원 수',
-                data: sortedWeekLeaveMemberData.map(item => item.count), // 탈퇴 회원 수 (Y축)
+                data: fillData(pastWeekDates, weekLeaveMemberData), // 탈퇴 회원 수 (Y축)
                 borderColor: 'rgba(255, 206, 86, 1)', // 탈퇴 회원 선 색상
                 backgroundColor: 'rgba(255, 206, 86, 0.2)', // 선 내부 색상
                 fill: true, // 선 내부 채우기
