42db99886e9c05b828d46116a9b337ec126b1410
codelily98 <codelily98@naver.com>
2025-01-02 12:03:07 +0900

    [FE] Fix: Scroll console.log 삭제

--- src/api/ResetScrollOnNavigate.jsx
+++ src/api/ResetScrollOnNavigate.jsx
@@ -10,7 +10,6 @@ const ResetScrollOnNavigate = () => {
 
         // 스크롤 복원
         document.body.style.overflow = "auto";
-        console.log("페이지 이동: 스크롤 초기화 및 overflow 복원");
     }, [location]);
 
     return null;
