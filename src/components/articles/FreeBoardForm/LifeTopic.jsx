4906f1148182f2359cb24969b23405bf21f9d30d
HyunSik <gustlr887@naver.com>
2025-01-02 15:37:43 +0900

    [FE] Ref: 달인게시판 일반유저도 보이게 (#578)
    * [FE] Ref: 달인게시판 일반유저도 보이게

--- src/components/articles/FreeBoardForm/LifeTopic.jsx
+++ src/components/articles/FreeBoardForm/LifeTopic.jsx
@@ -14,10 +14,7 @@ const LifeTopic = () => {
                     <li onClick={() => navigate('/article/review')}><Link to={'/article/review'}>최신리뷰</Link></li>
                     <li onClick={() => navigate('/article/free')}><Link to={'/article/free'}>자유 게시판</Link></li>
                     <li onClick={() => navigate('/article/qna')}><Link to={'/article/qna'}>Q&A</Link></li>
-                    {/* 조건부 렌더링: memberStatus가 ROLE_PRO 또는 ROLE_ADMIN일 경우만 보이도록 */}
-                    {(memberStatus === 'ROLE_PRO' || memberStatus === 'ROLE_ADMIN') && (
-                        <li onClick={() => navigate('/article/pro')}><Link to={'/article/pro'}>달인 게시판</Link></li>
-                    )}
+                    <li onClick={() => navigate('/article/pro')}><Link to={'/article/pro'}>달인 게시판</Link></li>
                 </ul>
             </section>
         </div>
