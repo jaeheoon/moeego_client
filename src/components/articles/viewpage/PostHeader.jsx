0d668d3e684fd8a963cbeffa4c700947afa89198
HyunSik <gustlr887@naver.com>
2025-01-03 14:33:35 +0900

    [FE] Fix: 글 상세 옵션버튼 (#590)
    * [FE] Fix: 글 상세 옵션버튼

--- src/components/articles/viewpage/PostHeader.jsx
+++ src/components/articles/viewpage/PostHeader.jsx
@@ -123,8 +123,7 @@ const PostHeader = ({ articleData, deleteArticle }) => {
                                 className="post-action-button"
                                 onClick={toggleDropdown}
                             >
-                                <img src="/image/3dots.png" alt="옵션" className="action-icon" />
-                            </button>
+                                <svg width="2rem" height="2rem" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000" class="bi bi-three-dots-vertical"><g id="SVGRepo_bgCarrier" strokeWidth="0"/><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/> </g></svg></button>
                             {isDropdownOpen && (
                                 <ul className="post-dropdown-menu">
                                     <li onClick={() => handleEdit(articleData.articleNo)}>글 수정</li>
